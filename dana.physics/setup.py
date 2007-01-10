43d8fcc1bd4fb7afecbb883c90e7cf8c6837944b
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2007-01-10 13:38:38 +0000

    fix numpy include directory

--- dana.physics/setup.py
+++ dana.physics/setup.py
@@ -10,12 +10,14 @@ if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 import glob
 from distutils.core import setup, Extension
 import distutils.sysconfig
-
+import numpy
 
 # Get core shared object filename
 from dana.core._core import __file__ as core
 include_dir = core[:core.find('python')]
-include_dir = os.path.normpath (os.path.join (include_dir, '../include/dana/'))
+include_dirs = [os.path.normpath (os.path.join (include_dir, '../include/dana/'))]
+include_dirs.append (numpy.get_include())
+
 
 print '-----------------------------------------------------'
 print 'Guessed include directory (based on package core) :'
@@ -30,7 +32,7 @@ physics_ext = Extension (
     'dana.physics._physics',
     sources = physics_srcs,
     libraries = ['boost_python'],
-    include_dirs =  [include_dir],
+    include_dirs =  include_dirs,
     extra_objects=[core]
 )
 
