8d1efa27d7748444739ede9e88e126a7a9245910
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2007-01-10 13:38:32 +0000

    fix numpy include directory

--- dana.cnft/setup.py
+++ dana.cnft/setup.py
@@ -10,12 +10,13 @@ if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 import glob
 from distutils.core import setup, Extension
 import distutils.sysconfig
-
+import numpy
 
 # Get core shared object filename
 from dana.core._core import __file__ as core
 include_dir = core[:core.find('python')]
-include_dir = os.path.normpath (os.path.join (include_dir, '../include/dana/'))
+include_dirs = [os.path.normpath (os.path.join (include_dir, '../include/dana/'))]
+include_dirs.append (numpy.get_include())
 
 print '-----------------------------------------------------'
 print 'Guessed include directory (based on package core) :'
@@ -30,7 +31,7 @@ cnft_ext = Extension (
     'dana.cnft._cnft',
     sources = cnft_srcs,
     libraries = ['boost_python'],
-    include_dirs =  [include_dir],
+    include_dirs =  include_dirs,
     extra_objects=[core]
 )
 
