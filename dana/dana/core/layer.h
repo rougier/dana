29346c539dadf674a8770c137efcfb706a6a373e
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2007-01-12 10:16:45 +0000

    Added Id tag

--- dana/dana/core/layer.h
+++ dana/dana/core/layer.h
@@ -5,6 +5,8 @@
 // modify it under the terms of the GNU General Public License as
 // published by the Free Software Foundation; either version 2 of the
 // License, or (at your option) any later version.
+//
+// $Id$
 
 #ifndef __DANA_CORE_LAYER_H__
 #define __DANA_CORE_LAYER_H__
