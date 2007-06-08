ad592616d4697a6afb683b6b88bc6ea3247cf872
fix <fix@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2007-06-08 13:08:58 +0000

    modified to used pkgconfig

--- dana.image/setup.py
+++ dana.image/setup.py
@@ -34,7 +34,6 @@ dana_image_ext = Extension (
         'dana.image._image',
         sources = dana_image_srcs,
         include_dirs=[numpy.get_include()] + pkgconfig('mirage')['include_dirs'],
-        library_dirs=['/users/cortex/fix/local/lib/'],
         libraries = ['boost_python', 'boost_thread'] + pkgconfig('mirage')['libraries']
         )
 
