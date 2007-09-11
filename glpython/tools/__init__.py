30398c7b3e33daa9dbb9ae8a221c36a06c686581
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2007-09-11 19:06:00 +0000

    Fixed framebuffer wrong type

--- glpython/tools/__init__.py
+++ glpython/tools/__init__.py
@@ -13,27 +13,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+import commands
+import sys
 
 def CheckLibrary (env, libs = []):
     """ Check for libraries using pkg-config """
+
+    ok = True
     for lib in libs:
         if env['VERBOSE']: print 'Checking for C library %s... ' % lib,
-        if os.system ('pkg-config --exists %s' % lib) == 1:
+        status, output = commands.getstatusoutput('pkg-config --exists %s' % lib)
+        if status:
             if env['VERBOSE']: print 'no'
-            sys.exit(1)
-        if env['VERBOSE']: print 'yes'
-        env.ParseConfig ('pkg-config --cflags --libs %s' % lib)
+            ok = False
+        else:
+            if env['VERBOSE']: print 'yes'
+            env.ParseConfig ('pkg-config --cflags --libs %s' % lib)
+    return ok
 
 def CheckModule (env, modules = []):
     """ Check for python modules """
+
+    ok = True
     for module in modules:
         if env['VERBOSE']: print 'Checking for Python package %s... ' % module,
         try:
             __import__ (module)
         except:
             if env['VERBOSE']: print 'no'
-            sys.exit(1)
+            ok = False
         else:
             if env['VERBOSE']: print 'yes'
+    return ok
 
