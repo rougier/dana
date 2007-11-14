100945947636a94f3f4e59074cd44dc41bc90737
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2007-11-14 15:58:32 +0000

    Updated doc

--- dana.core/doc/post_process.py
+++ dana.core/doc/post_process.py
@@ -1,6 +1,7 @@
 #! /bin/env python
 #
 import sys, os, glob
+import string
 from HTMLParser import HTMLParser
 
 path = 'manual/'
@@ -21,6 +22,7 @@ class Parser (HTMLParser):
         self.div_stack = 0
         self.nav_stack = -1
         self.in_title = False
+        self.in_h1 = False
         self.in_navigation = 0
 
     def handle_starttag (self, tag, attrs):
@@ -30,6 +32,10 @@ class Parser (HTMLParser):
         self.last_starttag = tag
         self.last_endtag = None
         
+
+        if tag == 'h1':
+            self.in_h1 = True
+
         if tag == 'div':
             self.div_stack += 1
 
@@ -44,18 +50,27 @@ class Parser (HTMLParser):
             for a,v in attrs:
                 if a == 'href':
                     self.prev  = v
+                elif a == 'title':
+                    self.prev_title = v
         elif tag == 'a' and ('rel', 'next') in attrs:
             for a,v in attrs:
                 if a == 'href':
                     self.next = v
+                elif a == 'title':
+                    self.next_title = v
         elif tag == 'a' and ('rel', 'contents') in attrs:
             for a,v in attrs:
                 if a == 'href':
                     self.contents = v
+                elif a == 'title':
+                    self.contents_title = v
+
         elif tag == 'a' and ('rel', 'parent') in attrs:
             for a,v in attrs:
                 if a == 'href':
                     self.parent = v
+                elif a == 'title':
+                    self.parent_title = v
 
     def handle_data (self,data):
         """
@@ -63,6 +78,8 @@ class Parser (HTMLParser):
         
         if self.in_title:
             self.title = data
+        elif self.in_h1:
+            pass
 
     def handle_endtag (self, tag):
         """
@@ -72,6 +89,8 @@ class Parser (HTMLParser):
 
         if tag == 'title':
             self.in_title = False
+        elif tag == 'h1':
+            self.in_h1 = False
         
         if tag == 'div':
             self.div_stack -= 1
@@ -83,14 +102,15 @@ class Parser (HTMLParser):
 
 
 for filename in glob.glob( os.path.join(path, '*.html') ):
+#for filename in glob.glob( os.path.join(path, 'node4.html') ):
     print "Processing %s" % filename
 
     parser = Parser()
 
-    f = open (filename)
-    for line in f:
-        parser.feed (line)
-    f.close()
+#    f = open (filename)
+#    for line in f:
+#        parser.feed (line)
+#    f.close()
 
     lines = []
     f = open (filename)
@@ -99,25 +119,32 @@ for filename in glob.glob( os.path.join(path, '*.html') ):
         if parser.last_starttag == 'body':
             lines.append ('<body>\n')
             lines.append ('<div class="page">\n')
-            lines.append ('<table width="100%" class="header"><tr>\n')
-            lines.append ('<td class="left" width="25%"><a href="index.html">DANA</a></td>\n')
-            lines.append ('<td class="right" width="75%%">%s</td>\n' % parser.title)
-            lines.append ('</tr></table>\n')
+#            lines.append ('<h1>%s</h1>\n' % parser.title)
+#            lines.append ('<table width="100%" class="header"><tr>\n')
+#            lines.append ('<td class="left" width="25%"><a href="index.html">DANA</a></td>\n')
+#            lines.append ('<td class="right" width="75%%">%s</td>\n' % parser.title)
+#            lines.append ('</tr></table>\n')
             lines.append ('<div class="content">\n')            
         elif parser.last_endtag == 'body':
             lines.append ('</div>\n')
             lines.append ('<table width="100%" class="footer"><tr>\n')
             if parser.prev:
-                lines.append ('<td class="left" width="50%%"><a href="%s">prev</a></td>\n' % (parser.prev))
+                lines.append ('<td class="left" width="50%%"><a href="%s">%s</a></td>\n' % (parser.prev, parser.prev_title))
             else:
                 lines.append ('<td class="left" width="50%"></td>\n')
             if parser.next:
-                lines.append ('<td class="right" width="50%%"><a href="%s">next</a></td>\n' % (parser.next))
+                lines.append ('<td class="right" width="50%%"><a href="%s">%s</a></td>\n' % (parser.next, parser.next_title))
             else:
                 lines.append ('<td class="right" width="50%"></td>\n')       
             lines.append ('</tr></table>\n')
             lines.append ('</div>\n')
             lines.append ('</body>\n')
+        elif parser.in_h1:
+            pass
+        elif line == "</h1>\n":
+            lines.append ('<h1>%s</h1>\n' % parser.title)
+            if hasattr(parser, 'parent_title'):
+                lines.append ('<div class="nav_right"><a href="%s">%s</a></div>\n' % (parser.parent, parser.parent_title))
         elif parser.in_navigation == 0:
             lines.append (line)
         elif parser.in_navigation == 1:
