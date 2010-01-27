00f43a8ab11cfe0ccd342d7d8be6ef147e4995b6
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2010-01-27 21:05:08 +0000

    Updated diffusion

--- dana/examples/diffusion.py
+++ dana/examples/diffusion.py
@@ -1,71 +1,53 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#   ____  _____ _____ _____ 
-#  |    \|  _  |   | |  _  |   DANA, Distributed Asynchronous Adaptive Numerical
-#  |  |  |     | | | |     |         Computing Framework
-#  |____/|__|__|_|___|__|__|         Copyright (C) 2009 INRIA  -  CORTEX Project
-#                         
-#  This program is free software: you can redistribute it and/or modify it under
-#  the terms of the GNU General Public License as published by the Free Software
-#  Foundation, either  version 3 of the  License, or (at your  option) any later
-#  version.
+# DANA, Distributed Asynchronous Adaptive Numerical Computing Framework
+# Copyright (c) 2009 Nicolas Rougier - INRIA - CORTEX Project
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either  version 3 of the  License, or (at your  option)
+# any later version.
 # 
-#  This program is  distributed in the hope that it will  be useful, but WITHOUT
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#  FOR  A  PARTICULAR PURPOSE.  See  the GNU  General  Public  License for  more
-#  details.
+# This program is  distributed in the hope that it will  be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR  A  PARTICULAR PURPOSE.  See  the GNU  General  Public 
+# License for  more details.
 # 
-#  You should have received a copy  of the GNU General Public License along with
-#  this program. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy  of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
 # 
-#  If you modify  this software, you should include a notice  giving the name of
-#  the person  performing the  modification, the date  of modification,  and the
-#  reason for such modification.
-# 
-#  Contact: 
-# 
-#      CORTEX Project - INRIA
-#      INRIA Lorraine, 
-#      Campus Scientifique, BP 239
-#      54506 VANDOEUVRE-LES-NANCY CEDEX 
-#      FRANCE
-# 
-import numpy, pylab
-import dana, dana.pylab
+# Contact:  CORTEX Project - INRIA
+#           INRIA Lorraine, 
+#           Campus Scientifique, BP 239
+#           54506 VANDOEUVRE-LES-NANCY CEDEX 
+#           FRANCE
+'''
+Heat equation simulation using forward difference / central difference (FTCS)
+method.
+'''
+import numpy, dana
+import matplotlib.pyplot as plt
 
-# Build groups
-# ______________________________________________________________________________
-n = 100
-G = dana.group((n,n))
-
-# Connections
-# ______________________________________________________________________________
-K = numpy.array([[1, 1, 1],
-                 [1, 0, 1],
-                 [1, 1, 1]])
-G.connect(G.V, K, 'N', shared=True)
-
-# Set Dynamic Neural Field equation
-# ______________________________________________________________________________
+n = 50
 k = 1
-G.dV = 'dt*k*(N/8-V)'
+G = dana.group((n,n))
+K = numpy.array([[0, 1, 0],
+                 [1, 0, 1],
+                 [0, 1, 0]])
+G.connect(G.V, K, 'N', shared=True)
+G.dV = 'dt*k*(N/4-V)'
 
-# Set input
-# ______________________________________________________________________________
 G.V = -numpy.random.ranf((n,n))
+for i in range(2000):
+    G.compute(dt=1)
+    G.V[0,:] = 0 
+    G.V[:,n-1] = G.V[n-1,:] = G.V[:,0] = 1
 
-# Run some iterations
-# ______________________________________________________________________________
-for i in range(200):
-    G.V[0,:] = G.V[n-1,:] = G.V[:,0] = 1
-    G.V[:,n-1] = -1
-    G.compute(1)
-G.V[0,:] = G.V[n-1,:] = G.V[:,0] = 1
-G.V[:,n-1] = -1
-
-# Display result using pylab
-# __________________________________________________________________________
-view = dana.pylab.view(G.V, vmin=-1, vmax=1,
-                       cmap=pylab.cm.hot, interpolation='bicubic')
-view.show()
+fig = plt.figure(figsize=(10,8))
+plt.imshow(G.V, cmap=plt.cm.hot, origin='lower',
+           extent=[0,n,0,n],
+           interpolation='bicubic', vmin=0, vmax=1)
+plt.grid()
+plt.colorbar()
+plt.show()
