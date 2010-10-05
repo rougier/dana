b7371fc5f3233656b00e31ff7e8c7c5d58c3d506
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2010-10-05 20:34:49 +0000

    Increased micro version number

--- common.py
+++ common.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # DANA - Distributed (Asynchronous) Numerical Adaptive computing framework
 # Copyright (C) 2009-2010  Nicolas P. Rougier
 #
 # Distributed under the terms of the BSD License. The full license is in
 # the file COPYING, distributed as part of this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 descr = '''
 DANA (Distributed (asynchronous) Numerical and Adaptive computing) is a python
 computing framework based on numpy and scipy libraries whose primary goals
@@ -33,7 +33,7 @@ PLATFORM            = 'any'
 
 MAJOR = 0
 MINOR = 3
-MICRO = 1
+MICRO = 2
 ALPHA = False
 BETA  = False
 DEV   = False
