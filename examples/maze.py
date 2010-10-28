e7ad960db38037883bdaeffff4a22bb73607b086
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2010-10-28 08:22:10 +0000

    Added a nicer solution display

--- examples/maze.py
+++ examples/maze.py
@@ -44,7 +44,7 @@ def maze(shape=(64,64), complexity=.75, density =.5):
     return Z
 
 
-n = 43
+n = 45
 a = 0.99
 Z = 1-maze((n,n))
 G = Group((n,n),'''V = I*maximum(maximum(maximum(maximum(V,E),W),N),S)
@@ -58,31 +58,33 @@ G.V[-2,-1] = 1
 run(n=5*(n+n))
 
 
-# Path finding by ascending gradient
-y,x = 0, 1
-dirs = [(0,-1), (0,+1), (-1,0), (+1,0)]
-S = Z.astype(float)
-for i in range(5*(n+n)):
-    S[y,x] = np.NaN
-    g = np.ones(4)*-1
-    if x > 0:            g[0] = G.V[y,x-1]
-    if x < G.shape[1]-1: g[1] = G.V[y,x+1]
-    if y > 0:            g[2] = G.V[y-1,x]
-    if y < G.shape[0]-1: g[3] = G.V[y+1,x]
-    a = np.argmax(g)
-    x,y  = x + dirs[a][1], y + dirs[a][0]
-
 
 # Visualization
-plt.figure(figsize=(15,7))
+plt.figure(figsize=(10,10))
 cmap = plt.cm.hot
 cmap.set_under('white')
 cmap.set_over('black')
 cmap.set_bad('blue')
-plt.subplot(1,2,1)
 plt.imshow(G.V, interpolation='nearest', cmap=cmap, vmin=0, vmax=1)
 plt.xticks([]), plt.yticks([])
-plt.subplot(1,2,2)
-plt.imshow(S, interpolation='nearest', cmap=cmap, vmin=0, vmax=1)
-plt.xticks([]), plt.yticks([])
+
+# Solution finding and drawing by ascending gradient
+y,x = 0, 1
+X,Y = [],[]
+dirs = [(0,-1), (0,+1), (-1,0), (+1,0)]
+S = Z.astype(float)
+for i in range(5*(n+n)):
+    Y.append(y), X.append(x)
+    neighbours = -np.ones(4)
+    if x > 0:            neighbours[0] = G.V[y,x-1]
+    if x < G.shape[1]-1: neighbours[1] = G.V[y,x+1]
+    if y > 0:            neighbours[2] = G.V[y-1,x]
+    if y < G.shape[0]-1: neighbours[3] = G.V[y+1,x]
+    a = np.argmax(neighbours)
+    x,y  = x + dirs[a][1], y + dirs[a][0]
+
+plt.scatter(X, Y, s=40.0, lw=2, color='k', marker='o',
+            alpha=1.0, edgecolors='k', facecolors='w')
+plt.axis( [-0.5,G.shape[1]-0.5, -0.5, G.shape[0]-0.5] )
+plt.title('Maze path finding using\n Bellman-Ford algorithm', fontsize=20)
 plt.show()
