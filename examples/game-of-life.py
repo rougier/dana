bbd17b60945b897223dfe2395ed5661615f099e0
rougier <rougier@1ceb54ca-f224-0410-9be4-bb156a55b06b>
2011-02-23 08:59:17 +0000

    Fix colormap

--- examples/game-of-life.py
+++ examples/game-of-life.py
@@ -19,17 +19,17 @@ from dana import *
 src = Group((50,100),
             '''V = maximum(0,1.0-(N<1.5)-(N>3.5)-(N<2.5)*(1-V)) : int
                N : float''')
-C = SharedConnection(src('V'), src('N'), np.array([[1., 1., 1.], 
+C = SparseConnection(src('V'), src('N'), np.array([[1., 1., 1.], 
                                                    [1., 0., 1.], 
                                                    [1., 1., 1.]]))
 src.V = rnd.randint(0, 2, src.shape)
 run(n=100)
 
 plt.figure(figsize=(8,4))
-plt.imshow(1-src.V, interpolation='nearest', extent=[0,100,0,50])
+plt.imshow(1-src.V, interpolation='nearest',
+           extent=[0,100,0,50], cmap=plt.cm.gray)
 plt.xticks(range(0,101,10)), plt.yticks(range(0,51,10))
 plt.grid()
-plt.savefig("game-of-life.png",dpi=75)
 plt.show()
 
 
