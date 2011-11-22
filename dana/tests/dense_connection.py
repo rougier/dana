e2d20cd74f0d73662d635e6dffb8513d7fc2438d
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2011-11-22 08:16:52 +0100

    Fix cecill website address in copyright notice

--- dana/tests/dense_connection.py
+++ dana/tests/dense_connection.py
@@ -10,8 +10,7 @@
 # This software is governed by the CeCILL license under French law and abiding
 # by the rules of distribution of free software. You can use, modify and/ or
 # redistribute the software under the terms of the CeCILL license as circulated
-# by CEA, CNRS and INRIA at the following URL
-# http://www.cecill.info/index.en.html.
+# by CEA, CNRS and INRIA at the following URL http://www.cecill.info.
 #
 # As a counterpart to the access to the source code and rights to copy, modify
 # and redistribute granted by the license, users are provided only with a
